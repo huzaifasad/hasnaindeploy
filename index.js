8bd2851e6c269b314e563211f5fd0c07a8d13945
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-20 06:09:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,30 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import addItemRoute from './Routes/addItemRoute.js';
 import getItemsRoute from './Routes/getItemsRoute.js';
 import deleteItemsRoute from './Routes/deleteItemsRoute.js';
 import updateItemsRoute from './Routes/updateItemsRoute.js';
 import { handleImageUpload } from './controllers/uploadImageController.js';
 import multer from 'multer';
-import bodyParser from 'body-parser';
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.listen(5000);
 app.use(cors({
-    origin: "http://localhost:3000", // Update to your frontend URL
-    methods: ["POST", "GET","PUT","DELETE"],
-    credentials: true
-  }));
+  origin: "http://localhost:3000", // Update to your frontend URL
+  methods: ["POST", "GET", "PUT", "DELETE"],
+  credentials: true
+}));
+
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+
+mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/your-database-name?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => {
     console.log('Connected to MongoDB');
   })
@@ -35,9 +38,11 @@ app.use('/', getItemsRoute);
 app.use('/', deleteItemsRoute);
 app.use('/', updateItemsRoute);
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// app.post('/upload', upload.single('image'), handleImageUpload);
+app.post('/upload', upload.single('image'), handleImageUpload);
 
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
