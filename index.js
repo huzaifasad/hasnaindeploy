189ab80c927a7d73c632b06167f8179f0a30b688
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-20 06:01:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-import addItemRoute from './Routes/addItemRoute.js';
+// import addItemRoute from './Routes/addItemRoute.js';
 import getItemsRoute from './Routes/getItemsRoute.js';
-import deleteItemsRoute from './Routes/deleteItemsRoute.js';
-import updateItemsRoute from './Routes/updateItemsRoute.js';
-import { handleImageUpload } from './controllers/uploadImageController.js';
+// import deleteItemsRoute from './Routes/deleteItemsRoute.js';
+// import updateItemsRoute from './Routes/updateItemsRoute.js';
+// import { handleImageUpload } from './controllers/uploadImageController.js';
 import multer from 'multer';
 
 const app = express();
@@ -14,7 +14,9 @@ const port = 5000;
 
 app.use(express.json());
 app.use(cors());
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
@@ -23,16 +25,14 @@ mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/?r
     console.error('MongoDB connection error:', err);
   });
 
-app.use('/', addItemRoute);
+// app.use('/', addItemRoute);
 app.use('/', getItemsRoute);
-app.use('/', deleteItemsRoute);
-app.use('/', updateItemsRoute);
+// app.use('/', deleteItemsRoute);
+// app.use('/', updateItemsRoute);
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
-app.post('/upload', upload.single('image'), handleImageUpload);
+// app.post('/upload', upload.single('image'), handleImageUpload);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
