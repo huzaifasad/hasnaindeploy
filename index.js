5078c2f9aeeae06c0d45c7da22660287dc3d1c64
Huzaifa Saad <140094374+huzaifasad@users.noreply.github.com>
2023-12-20 06:06:09 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
-// import addItemRoute from './Routes/addItemRoute.js';
+import addItemRoute from './Routes/addItemRoute.js';
 import getItemsRoute from './Routes/getItemsRoute.js';
-// import deleteItemsRoute from './Routes/deleteItemsRoute.js';
-// import updateItemsRoute from './Routes/updateItemsRoute.js';
-// import { handleImageUpload } from './controllers/uploadImageController.js';
+import deleteItemsRoute from './Routes/deleteItemsRoute.js';
+import updateItemsRoute from './Routes/updateItemsRoute.js';
+import { handleImageUpload } from './controllers/uploadImageController.js';
 import multer from 'multer';
 import bodyParser from 'body-parser';
 
@@ -30,14 +30,14 @@ mongoose.connect('mongodb+srv://husnaink467:5262@cluster0.ghk3bf3.mongodb.net/?r
     console.error('MongoDB connection error:', err);
   });
 
-// app.use('/', addItemRoute);
+app.use('/', addItemRoute);
 app.use('/', getItemsRoute);
-// app.use('/', deleteItemsRoute);
-// app.use('/', updateItemsRoute);
+app.use('/', deleteItemsRoute);
+app.use('/', updateItemsRoute);
 
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-// app.post('/upload', upload.single('image'), handleImageUpload);
+app.post('/upload', upload.single('image'), handleImageUpload);
 
 
